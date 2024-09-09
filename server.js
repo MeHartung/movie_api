2cb564038509096295871fce0b379c23b1695c0b
Anna Hartung <annamailda@gmail.com>
2024-09-09 18:42:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -184,6 +184,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 
 // READ 
 // Get all movies
+/* 
 app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Movies.find()
         .then((movies) => {
@@ -194,6 +195,18 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), async (req,
             res.status(500).send('Error: ' + error);
         });
 });
+*/
+
+app.get('/movies', async (req, res) => {
+    try {
+        const movies = await Movies.find();
+        res.status(200).json(movies);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
 
 // Get a user by username
 app.get('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
